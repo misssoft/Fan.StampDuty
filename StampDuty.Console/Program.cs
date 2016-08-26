82596b4205631ed1c128e441e044bd3be411b88b
Fan Yang <fan.yang.turner@live.co.uk>
2016-08-26 23:39:50 +0100

    Add region label

--- StampDuty.Console/Program.cs
+++ StampDuty.Console/Program.cs
@@ -18,14 +18,16 @@ namespace StampDuty.Console
                 var isFreehold = System.Console.ReadLine();
 
                 if (isFreehold == "1")
+                #region FreeHold
                 {
                     System.Console.WriteLine("Residential or Non-residential");
                     System.Console.WriteLine("1. Residential");
                     System.Console.WriteLine("2. Non-residential");
                     var isResidential = System.Console.ReadLine();
 
-                    if (isFreehold == "1")
+                    if (isResidential == "1")
                     {
+                        #region Residential
                         System.Console.WriteLine("Transaction Date");
                         System.Console.WriteLine("1. After 01/04/2016");
                         System.Console.WriteLine("2. Before 01/04/2016");
@@ -33,6 +35,7 @@ namespace StampDuty.Console
 
                         if (transactionDate == "1")
                         {
+                        #region After2016
                             System.Console.WriteLine("Individual or Joined");
                             System.Console.WriteLine("1. Individual");
                             System.Console.WriteLine("2. Joined");
@@ -40,6 +43,7 @@ namespace StampDuty.Console
 
                             if (isIndividual == "1")
                             {
+                                #region Individual
                                 System.Console.WriteLine("Second Home");
                                 System.Console.WriteLine("1. Yes");
                                 System.Console.WriteLine("2. No");
@@ -47,6 +51,7 @@ namespace StampDuty.Console
 
                                 if (isSecond == "1")
                                 {
+                                    #region SecondHome
                                     System.Console.WriteLine("Replacing main property");
                                     System.Console.WriteLine("1. Yes");
                                     System.Console.WriteLine("2. No");
@@ -54,6 +59,7 @@ namespace StampDuty.Console
 
                                     if (isReplacing == "1")
                                     {
+                                        #region replacingmainhome
                                         System.Console.WriteLine("Property Price:");
                                         var price = System.Console.ReadLine();
 
@@ -64,9 +70,11 @@ namespace StampDuty.Console
                                         System.Console.WriteLine($"TransactionDate: {transactionDate}");
                                         System.Console.WriteLine($"Residential: {isResidential}");
                                         System.Console.WriteLine($"Freehold: {isFreehold}");
+                                        #endregion
                                     }
                                     else
                                     {
+                                        #region keepasSecond
                                         System.Console.WriteLine("Property Price:");
                                         var price = System.Console.ReadLine();
                                         System.Console.WriteLine($"Price: {price}");
@@ -76,10 +84,13 @@ namespace StampDuty.Console
                                         System.Console.WriteLine($"TransactionDate: {transactionDate}");
                                         System.Console.WriteLine($"Residential: {isResidential}");
                                         System.Console.WriteLine($"Freehold: {isFreehold}");
+                                        #endregion
                                     }
+                                    #endregion
                                 }
                                 else
                                 {
+                                    #region FirstTimeBuyer
                                     System.Console.WriteLine("Property Price:");
                                     var price = System.Console.ReadLine();
                                     System.Console.WriteLine($"Price: {price}");
@@ -88,10 +99,13 @@ namespace StampDuty.Console
                                     System.Console.WriteLine($"TransactionDate: {transactionDate}");
                                     System.Console.WriteLine($"Residential: {isResidential}");
                                     System.Console.WriteLine($"Freehold: {isFreehold}");
+                                    #endregion
                                 }
+                                #endregion
                             }
                             else
                             {
+                                #region organisation
                                 System.Console.WriteLine("Property Price:");
                                 var price = System.Console.ReadLine();
                                 System.Console.WriteLine($"Price: {price}");
@@ -99,130 +113,43 @@ namespace StampDuty.Console
                                 System.Console.WriteLine($"TransactionDate: {transactionDate}");
                                 System.Console.WriteLine($"Residential: {isResidential}");
                                 System.Console.WriteLine($"Freehold: {isFreehold}");
+                                #endregion
                             }
+                            #endregion
                         }
                         else
                         {
+                        #region Before2016
                             System.Console.WriteLine("Property Price:");
                             var price = System.Console.ReadLine();
                             System.Console.WriteLine($"Price: {price}");
                             System.Console.WriteLine($"TransactionDate: {transactionDate}");
                             System.Console.WriteLine($"Residential: {isResidential}");
                             System.Console.WriteLine($"Freehold: {isFreehold}");
+                            #endregion
                         }
+                        #endregion
                     }
                     else
                     {
+                        #region Non-Residential
                         System.Console.WriteLine("Property Price:");
                         var price = System.Console.ReadLine();
                         System.Console.WriteLine($"Price: {price}");
                         System.Console.WriteLine($"Residential: {isResidential}");
                         System.Console.WriteLine($"Freehold: {isFreehold}");
+                        #endregion   
                     }
+                #endregion
                 }
                 else
                 {
-                    System.Console.WriteLine("Residential or Non-residential");
-                    System.Console.WriteLine("1. Residential");
-                    System.Console.WriteLine("2. Non-residential");
-                    var isResidential = System.Console.ReadLine();
-
-                    if (isFreehold == "1")
-                    {
-                        System.Console.WriteLine("Transaction Date");
-                        System.Console.WriteLine("1. After 01/04/2016");
-                        System.Console.WriteLine("2. Before 01/04/2016");
-                        var transactionDate = System.Console.ReadLine();
-
-                        if (transactionDate == "1")
-                        {
-                            System.Console.WriteLine("Individual or Joined");
-                            System.Console.WriteLine("1. Individual");
-                            System.Console.WriteLine("2. Joined");
-                            var isIndividual = System.Console.ReadLine();
-
-                            if (isIndividual == "1")
-                            {
-                                System.Console.WriteLine("Second Home");
-                                System.Console.WriteLine("1. Yes");
-                                System.Console.WriteLine("2. No");
-                                var isSecond = System.Console.ReadLine();
-
-                                if (isSecond == "1")
-                                {
-                                    System.Console.WriteLine("Replacing main property");
-                                    System.Console.WriteLine("1. Yes");
-                                    System.Console.WriteLine("2. No");
-                                    var isReplacing = System.Console.ReadLine();
-
-                                    if (isReplacing == "1")
-                                    {
-                                        System.Console.WriteLine("Property Price:");
-                                        var price = System.Console.ReadLine();
-
-                                        System.Console.WriteLine($"Price: {price}");
-                                        System.Console.WriteLine($"Replacing main property: {isReplacing}");
-                                        System.Console.WriteLine($"Second Home: {isSecond}");
-                                        System.Console.WriteLine($"Individual: {isIndividual}");
-                                        System.Console.WriteLine($"TransactionDate: {transactionDate}");
-                                        System.Console.WriteLine($"Residential: {isResidential}");
-                                        System.Console.WriteLine($"Freehold: {isFreehold}");
-                                    }
-                                    else
-                                    {
-                                        System.Console.WriteLine("Property Price:");
-                                        var price = System.Console.ReadLine();
-                                        System.Console.WriteLine($"Price: {price}");
-                                        System.Console.WriteLine($"Replacing main property: {isReplacing}");
-                                        System.Console.WriteLine($"Second Home: {isSecond}");
-                                        System.Console.WriteLine($"Individual: {isIndividual}");
-                                        System.Console.WriteLine($"TransactionDate: {transactionDate}");
-                                        System.Console.WriteLine($"Residential: {isResidential}");
-                                        System.Console.WriteLine($"Freehold: {isFreehold}");
-                                    }
-                                }
-                                else
-                                {
-                                    System.Console.WriteLine("Property Price:");
-                                    var price = System.Console.ReadLine();
-                                    System.Console.WriteLine($"Price: {price}");
-                                    System.Console.WriteLine($"Second Home: {isSecond}");
-                                    System.Console.WriteLine($"Individual: {isIndividual}");
-                                    System.Console.WriteLine($"TransactionDate: {transactionDate}");
-                                    System.Console.WriteLine($"Residential: {isResidential}");
-                                    System.Console.WriteLine($"Freehold: {isFreehold}");
-                                }
-                            }
-                            else
-                            {
-                                System.Console.WriteLine("Property Price:");
-                                var price = System.Console.ReadLine();
-                                System.Console.WriteLine($"Price: {price}");
-                                System.Console.WriteLine($"Individual: {isIndividual}");
-                                System.Console.WriteLine($"TransactionDate: {transactionDate}");
-                                System.Console.WriteLine($"Residential: {isResidential}");
-                                System.Console.WriteLine($"Freehold: {isFreehold}");
-                            }
-                        }
-                        else
-                        {
-                            System.Console.WriteLine("Property Price:");
-                            var price = System.Console.ReadLine();
-                            System.Console.WriteLine($"Price: {price}");
-                            System.Console.WriteLine($"TransactionDate: {transactionDate}");
-                            System.Console.WriteLine($"Residential: {isResidential}");
-                            System.Console.WriteLine($"Freehold: {isFreehold}");
-                        }
-                    }
-                    else
-                    {
-                        System.Console.WriteLine("Property Price:");
-                        var price = System.Console.ReadLine();
-                        System.Console.WriteLine($"Price: {price}");
-                        System.Console.WriteLine($"Residential: {isResidential}");
-                        System.Console.WriteLine($"Freehold: {isFreehold}");
-                    }
-
+                #region Leashold
+                    System.Console.WriteLine("Property Price:");
+                    var price = System.Console.ReadLine();
+                    System.Console.WriteLine($"Price: {price}");
+                    System.Console.WriteLine($"Freehold: {isFreehold}");
+                    #endregion
                 }
                 System.Console.ReadLine();
             }
